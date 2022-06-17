3fceb8f6ef9a56c72d5b65bc3d1b02042e07640d
Archisketch-Ben <ben@archisketch.com>
2022-06-17 14:21:41 +0900

    chore: set git hook & prettier

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-import "./env.js"
-import { Client } from "@notionhq/client"
+import "./env.js";
+import { Client } from "@notionhq/client";
 
-console.log(process.env.NOTION_KEY);
-console.log(process.env.NOTION_QA_DB_KEY);
+const notion = new Client({ auth: process.env.NOTION_KEY });
 
-const notion = new Client({ auth: process.env.NOTION_KEY })
-
-const databaseId = process.env.NOTION_QA_DB_KEY
+const databaseId = process.env.NOTION_QA_DB_KEY;
 
 async function addItem(text) {
   try {
     const response = await notion.pages.create({
       parent: { database_id: databaseId },
       properties: {
-        title: { 
-          title:[
+        title: {
+          title: [
             {
-              "text": {
-                "content": text
-              }
-            }
-          ]
-        }
+              text: {
+                content: text,
+              },
+            },
+          ],
+        },
       },
-    })
-    console.log(response)
-    console.log("Success! Entry added.")
+    });
+    console.log(response);
+    console.log("Success! Entry added.");
   } catch (error) {
-    console.error(error.body)
+    console.error(error.body);
   }
 }
 
-addItem("Yurts in Big Sur, California")
+addItem("Yurts in Big Sur, California");
